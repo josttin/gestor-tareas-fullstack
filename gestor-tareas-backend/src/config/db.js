deea9a660d5ccce0825c4b7f67a24a2ca368de15
josttin <josttin2003@gmail.com>
2025-07-17 17:58:17 -0500

    chore: Configurar entorno local con Docker y PostgreSQL

--- gestor-tareas-backend/src/config/db.js
+++ gestor-tareas-backend/src/config/db.js
@@ -1,29 +1,23 @@
 // src/config/db.js
-import mysql from "mysql2/promise";
-import pg from "pg";
+import pg from "pg"; // Solo necesitamos 'pg' ahora
 import "dotenv/config";
 
-// Usamos el Pool de 'pg' para producción (Render) y el de 'mysql2' para desarrollo
 let pool;
 
 if (process.env.NODE_ENV === "production") {
-  // Configuración para PostgreSQL en Render
+  // Configuración para Render (no cambia)
   pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    ssl: { rejectUnauthorized: false },
   });
 } else {
-  // Configuración para MySQL en local
-  pool = mysql.createPool({
+  // Configuración para LOCAL (ahora apunta a nuestro Docker)
+  pool = new pg.Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+    port: process.env.DB_PORT,
   });
 }
 
